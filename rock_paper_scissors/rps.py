80a98ffc9558fd1550a95150648cb3c0599202cc
kjohna <43828763+kjohna@users.noreply.github.com>
2019-05-02 14:07:00 -0600

    rps passing tests

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,28 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    if n == 0:
+        return [[]]
+    else:
+        return rps_helper(rock_paper_scissors(n-1))
+
+
+def rps_helper(games):
+    moves = ['rock', 'paper', 'scissors']
+    result = []
+    for game in games:
+        for move in moves:
+            new_game = game[:]
+            new_game.append(move)
+            result.append(new_game)
+    return result
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
